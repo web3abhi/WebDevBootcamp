b518ddd0f0c965d2c63f005c93978ef7a4305e71
Abhishek Sharma <arst2196@gmail.com>
2017-01-17 18:03:07 +0530

    Add files via upload

---
+++ 08 Intro to Js/ageCalc.js
@@ -0,0 +1,2 @@
+var age = prompt("Enter your age");
+alert("you are approx "+ age*365.25 +" days old");
---
+++ 08 Intro to Js/jsStalker.js
@@ -0,0 +1,5 @@
+var firstname = prompt("Enter your first name");
+var lastname = prompt("Enter your last name");
+var age = prompt("Enter your age");
+console.log("your name is "+firstname+" "+lastname);
+console.log("you are "+age+" years old");
